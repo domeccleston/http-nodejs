92275e22894fdbc3591d8c316b3d3f528e49cb26
Dom <domeccleston@gmail.com>
2023-03-14 15:57:39 +0000

    update

--- server.mjs
+++ server.mjs
@@ -1,6 +1,7 @@
-import { createServer } from 'http';
+import { createServer } from "http";
 
 createServer((req, res) => {
-  res.write('Hello World!');
+  console.log(req.body);
+  res.write("Hello World!");
   res.end();
-}).listen(process.env.PORT);
+}).listen(process.env.PORT || 4000);
