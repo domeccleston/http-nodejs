7d05af15c96f26fe2df78faef124c10e6dd7e934
Dom <domeccleston@gmail.com>
2023-03-14 16:03:16 +0000

    post

--- server.mjs
+++ server.mjs
@@ -1,7 +1,13 @@
-import { createServer } from "http";
+import express from "express";
 
-createServer((req, res) => {
-  console.log(req.body);
-  res.write("Hello World!");
-  res.end();
-}).listen(process.env.PORT || 4000);
+const app = express();
+
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`Server listening on port ${process.env.PORT || 4000}`);
+});
